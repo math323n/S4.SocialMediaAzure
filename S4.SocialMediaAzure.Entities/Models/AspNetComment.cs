ba7b78714c0c4fef8c9ce23e5b2c276a742edd5c
Troll Coder <math323n@edu.campusvejle.dk>
2020-11-05 11:38:52 +0100

    added entities

---
+++ S4.SocialMediaAzure.Entities/Models/AspNetComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace S4.SocialMediaAzure.Entities.Models
+{
+    public partial class AspNetComment
+    {
+        public int PkId { get; set; }
+        public string FkUserId { get; set; }
+        public int FkPostId { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsEdited { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public virtual AspNetPost FkPost { get; set; }
+        public virtual AspNetUser FkUser { get; set; }
+    }
+}
---
+++ S4.SocialMediaAzure.Entities/Models/AspNetPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace S4.SocialMediaAzure.Entities.Models
+{
+    public partial class AspNetPost
+    {
+        public AspNetPost()
+        {
+            AspNetComments = new HashSet<AspNetComment>();
+        }
+
+        public int PkId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsEdited { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public string FkUserId { get; set; }
+
+        public virtual AspNetUser FkUser { get; set; }
+        public virtual ICollection<AspNetComment> AspNetComments { get; set; }
+    }
+}
