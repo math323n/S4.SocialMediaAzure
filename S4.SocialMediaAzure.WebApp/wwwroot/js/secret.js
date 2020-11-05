2c16394c74689f0451b6c18f10491478ae9355c0
Troll Coder <math323n@edu.campusvejle.dk>
2020-11-05 14:25:18 +0100

    Added styling

---
+++ S4.SocialMediaAzure.WebApp/wwwroot/js/secret.js
@@ -0,0 +1,14 @@
+﻿const pressed = [];
+const secretCode = 'pony';
+
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        console.log('Congratulations, you won.');
+        cornify_add();
+    }
+    console.log(pressed);
+});
